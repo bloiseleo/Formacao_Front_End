00755dcbefad53e6e390c874871d503a51a8c160
Leonardo C. S. Bloise <leonardocsbloise@gmail.com>
2022-12-15 16:45:07 -0300

    feat: finalizando a primeira página

--- javascript/aula1/main.js
+++ javascript/aula1/main.js
@@ -1,3 +1,9 @@
-document.querySelector("tecla_pom").addEventListener('click', (event) => {
-    console.log(event)
+const teclas = document.querySelectorAll(".tecla")
+
+teclas.forEach(tecla => {
+    const classOfTecla = tecla.classList
+    const nomeTecla = classOfTecla[1];
+    const idSom = `#som_${nomeTecla}`
+    const audioElement = document.querySelector(idSom)
+    tecla.addEventListener('click', () => audioElement.play())
 })
